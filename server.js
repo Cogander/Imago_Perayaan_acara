29b73c52e4b2480f5e76cec088d1f880b0f3a008
Cogander <ivanderchristy13@gmail.com>
2026-03-04 17:53:13 +0700

    Refactor server.js for improved structure and clarity

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
-require("dotenv").config()
-const express = require("express")
-const mongoose = require("mongoose")
-const cors = require("cors")
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
-const app = express()
+const authRoutes = require("./routes/authRoutes");
 
-app.use(cors())
-app.use(express.json())
-app.use(express.static("public"))
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGO_URI)
-.then(()=>console.log("MongoDB Connected"))
-.catch(err=>console.log(err))
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
-app.use("/api/pesanan", require("./routes/orderRoutes"))
-app.use("/api/admin", require("./routes/adminRoutes"))
+app.use("/api/auth", authRoutes);
 
-app.listen(5000, () => {
-  console.log("Server berjalan di port 5000")
-})
+app.listen(process.env.PORT, () => {
+  console.log("Server running on port " + process.env.PORT);
+});
